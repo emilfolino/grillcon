08bafc50dae265448ec4be34e9d6906f49ba000c
Emil Folino BTH MAC <emilfolino@gmail.com>
2017-05-03 11:59:32 +0200

    Started in rust and almost done with ruby

---
+++ tsp.rb
@@ -0,0 +1,66 @@
+start = Time.now
+
+universities = {
+    'BTH' => {"lat" => 56.18, "long" => 15.59},
+    'Uppsala' => {"lat" => 59.85, "long" => 17.63},
+    'Lund' => {"lat" => 55.71, "long" => 13.20},
+    # 'KTH' => {"lat" => 59.34, "long" => 18.07},
+    # 'Chalmers' => {"lat" => 57.68, "long" => 11.97},
+    # 'Luleå' => {"lat" => 65.61, "long" => 22.14},
+    # 'Umeå' => {"lat" => 63.82, "long" => 20.30},
+    # 'Linköping' => {"lat" => 58.39, "long" => 15.57},
+    # 'Karlstad' => {"lat" => 59.40, "long" => 13.58},
+    # 'Örebro' => {"lat" => 59.25, "long" => 15.24}
+}
+
+university_names = universities.keys()
+permutations = {}
+
+N = university_names.length
+p = [N]*(N + 1)
+
+i = 0
+while i < N
+    p[i] = p[i] - 1
+
+    j = i%2 * p[i]
+
+    tmp = university_names[i]
+    university_names[i] = university_names[j]
+    university_names[j] = tmp
+
+    permutations[university_names.join] = university_names.clone
+    i = 1
+    while p[i] == 0
+        p[i] = i
+        i += 1
+    end
+end
+
+shortest_trip = 1000000
+shortest_trip_universities = {}
+
+permutations.each do |key, trip|:
+    trip_length = 0
+    trip.each do |city|
+        lat1 = universities[permutations[trip][i + 1]]["lat"]
+        lat0 = universities[permutations[trip][i]]["lat"]
+        long1 = universities[permutations[trip][i + 1]]["long"]
+        long0 = universities[permutations[trip][i]]["long"]
+        trip_length += sqrt((lat1 - lat0)*(lat1 - lat0) + (long1 - long0)*(long1 - long0))
+    end
+
+    if trip_length < shortest_trip:
+        shortest_trip = trip_length
+        shortest_trip_universities = permutations[trip]
+    end
+end
+
+
+finish = Time.now
+
+# print("Shortest trip length: " + str(shortest_trip))
+# print(" --> ".join(shortest_trip_universities))
+
+diff = finish - start
+puts "Execution time: " + diff.to_s
--- tsp.rs
+++ tsp.rs
@@ -1,8 +1,74 @@
-fn main() {
-    // Points are universities in Sweden
-    // Order is: BTH, Uppsala, Lund, KTH, Chalmers, Luleå, Umeå,
-    // Linköping, Karlstad, Örebro
-    let universities: [(f32, f32); 10] = [(56.18, 15.59), (59.85, 17.63), (55.71, 13.20), (59.34, 18.07), (57.68, 11.97), (65.61, 22.14), (63.82, 20.30), (58.39, 15.57), (59.40, 13.58), (59.25, 15.24)];
+use std::collections::HashMap;
 
-    println!("{}", universities[0].0);
+fn main() {
+    let mut universities = HashMap::new();
+    universities.insert("BTH", (56.18, 15.59));
+    universities.insert("Uppsala", (59.85, 17.63));
+    universities.insert("Lund", (55.71, 13.20));
+    universities.insert("KTH", (59.34, 18.07));
+    universities.insert("Chalmers", (57.68, 11.97));
+    universities.insert("Luleå", (65.61, 22.14));
+    universities.insert("Umeå", (63.82, 20.30));
+    universities.insert("Linköping", (58.39, 15.57));
+    universities.insert("Karlstad", (59.40, 13.58));
+    universities.insert("Örebro", (59.25, 15.24));
+
+    let university_names = universities.keys();
+
+    // let mut permutations = [];
+
+    let n: usize = university_names.len();
+
+    println!("{}", n);
+    let p: [i32; ] = [n as i32; ];
+    let i: i32 = 0;
+
+    // while ($i < $N) {
+    //     $p[$i]--;
+    //
+    //     $j = $i%2 * $p[$i];
+    //
+    //     $tmp = $university_names[$i];
+    //     $university_names[$i] = $university_names[$j];
+    //     $university_names[$j] = $tmp;
+    //
+    //     $permutations[implode("", array_values($university_names))] = $university_names;
+    //
+    //     $i = 1;
+    //     while ($p[$i] == 0) {
+    //         $p[$i] = $i;
+    //         $i++;
+    //     }
+    // }
+    //
+    // $shortest_trip = PHP_INT_MAX;
+    // $shortest_trip_universities = [];
+    //
+    // foreach ($permutations as $key => $trip) {
+    //     $trip_length = 0;
+    //
+    //     for ($i = 0; $i < count($trip) - 1; $i++) {
+    //         $lat1 = $universities[$trip[$i + 1]]["lat"];
+    //         $lat0 = $universities[$trip[$i]]["lat"];
+    //         $long1 = $universities[$trip[$i + 1]]["long"];
+    //         $long0 = $universities[$trip[$i]]["long"];
+    //         $trip_length += sqrt(($lat1 - $lat0)*($lat1 - $lat0) + ($long1 - $long0)*($long1 - $long0));
+    //     }
+    //
+    //     if ($trip_length < $shortest_trip) {
+    //         $shortest_trip = $trip_length;
+    //         $shortest_trip_universities = $trip;
+    //     }
+    // }
+    //
+    // $time_end = microtime(true);
+    //
+    // $execution_time = ($time_end - $time_start);
+    //
+    //
+    //
+    // echo "Shortest trip length: " . $shortest_trip . "\n";
+    // echo implode(" --> ", $shortest_trip_universities) . "\n";
+    // echo "Total Execution Time: ".$execution_time." seconds\n";
+
 }
